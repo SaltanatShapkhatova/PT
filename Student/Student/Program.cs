b6865ac80d9b981d6cfa0e400f22b5dc75be4380
Saltanat Shapkhatova <0604shapkhatova@gmail.com>
2016-01-25 03:26:25 +0600

    added prime number

---
+++ Student/Student/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string firstname, lastname;
+        public int gpa;
+        public Student()
+        {
+            gpa = 0;
+            firstname = lastname = "";
+        }
+       
+        public override string ToString()
+        {
+            return firstname + " " + lastname + " " + "," + gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            int c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = int.Parse(Console.ReadLine());
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.ReadKey();
+        }
+    }
+}
